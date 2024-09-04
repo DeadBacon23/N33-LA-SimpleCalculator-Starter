42dc4248cc70fb265dd51b01c183bc336d03ed40
MattysLeduc <mattys.leduc@gmail.com>
2024-09-04 12:14:43 -0400

    Requirement 4: All the tests cases for all the operations

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -29,5 +29,93 @@ namespace SimpleCalculator.Test.Unit
             Assert.AreEqual(3, result);
 
         }
+
+        [TestMethod]
+        public void SubstractTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("-", number2, number1);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void SubstractTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("minus", number2, number1);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("*", number2, number1);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("multiplied", number2, number1);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 4;
+            double result = _calculatorEngine.Calculate("/", number2, number1);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 4;
+            double result = _calculatorEngine.Calculate("divide", number2, number1);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void PowerOfTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 4;
+            double result = _calculatorEngine.Calculate("^", number2, number1);
+            Assert.AreEqual(16, result);
+
+        }
+
+        [TestMethod]
+        public void PowerOfTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 4;
+            double result = _calculatorEngine.Calculate("power", number2, number1);
+            Assert.AreEqual(16, result);
+
+        }
     }
 }
